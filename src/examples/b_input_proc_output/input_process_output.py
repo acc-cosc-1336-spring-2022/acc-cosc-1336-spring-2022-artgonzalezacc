99b9dfba580c19991613aa8327fb98c34602f837
artgonzalezacc <arturo.gonzalez@austincc.edu>
2022-03-22 16:39:18 -0500

    Ex: input out proc

--- src/examples/b_input_proc_output/input_process_output.py
+++ src/examples/b_input_proc_output/input_process_output.py
@@ -5,3 +5,33 @@ def display_output():
 def test_config():
     return True
 
+def add_numbers(num1, num2):#parameters give us flexibility/reuse
+    return num1 + num2
+
+def add_some_numbers():
+    return 5 + 5
+
+def multiply_numbers():
+    return 5 * 5
+
+def integer_division(num1, num2):
+    return num1 // num2
+
+def float_division(num1, num2):
+    return num1 / num2
+
+def operator_precedence_1(num1, num2, num3):#no parenthesis
+    return num1 + num2 / num3
+
+def operator_precedence_2(num1, num2, num3):#w parenthesis
+    return (num1 + num2) / num3
+
+def exponentiate_me(num1, num2):
+    return num1 ** num2
+
+def get_remainder(num1, num2):
+    return num1 % num2
+
+
+
+
--- src/examples/b_input_proc_output/main.py
+++ src/examples/b_input_proc_output/main.py
@@ -1,3 +1,7 @@
 import input_process_output
 
-input_process_output.display_output()
+num0 = 5.5 # numbering system int
+print(num0) #prints value of num0 variable
+#print('num0')#prints the sequence num0
+num0 = int(input('number'))#we want a number
+
