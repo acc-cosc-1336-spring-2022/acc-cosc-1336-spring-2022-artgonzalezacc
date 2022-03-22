bae4afdc52abcc220f6cbf4262b7f0bb57072398
artgonzalezacc <arturo.gonzalez@austincc.edu>
2022-03-22 16:45:12 -0500

    String examples

--- src/examples/h_strings/main.py
+++ src/examples/h_strings/main.py
@@ -1,1 +1,22 @@
 #main program
+import strings
+
+#strings.hello_world()
+#strings.loop_string("hello")
+#strings.loop_string_w_while("hello")
+
+#str = "Four score and seven years ago"
+#sub_string = "sevenn"
+
+#if strings.string_sub_string(str, sub_string):
+#print(sub_string + " in " + str)
+#else:
+# print(sub_string + " not in " + str)
+
+str = "Four score and seven years ago"
+
+word_list = str.split()
+print(word_list)
+
+for word in word_list:
+    print(word)
--- src/examples/h_strings/strings.py
+++ src/examples/h_strings/strings.py
@@ -1,2 +1,29 @@
 def test_config():
     return True
+
+def hello_world():
+    hello = "hello world!12" #string
+    print(hello[0])
+    print(hello)
+
+def loop_string(str):
+    for c in str:#hello
+        print(c)
+
+def loop_string_w_while(str):
+    indx = 0
+
+    while indx < len(str):
+        print(str[indx])
+        indx = indx + 1
+
+def concat_strings(str1, str2):
+    return str1 + str2
+
+def slice_string(str):
+    return str[6:10]
+
+def string_sub_string(str, sub_string):
+    return sub_string in str
+
+
