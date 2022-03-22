40db4ed85b8abf2fafe596af970b9e582412862b
artgonzalezacc <arturo.gonzalez@austincc.edu>
2022-03-22 16:43:16 -0500

    Function examples

--- src/examples/e_functions/main.py
+++ src/examples/e_functions/main.py
@@ -1,1 +1,11 @@
 #main program
+import value_return_functions
+
+#val0 = 0 #global variable
+#value_return_functions.main(val0)
+#value_return_functions.use_global()
+
+#print(value_return_functions.VAL3)
+value_return_functions.display_random_numbers(1,50, 10)
+
+#value_return_functions.display_random_numbers(500,550, 10)
